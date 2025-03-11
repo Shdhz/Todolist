84ef537d5debe9f0c37e1c53921e1f16cdc9eec2
Shdhz <sicucut43@gmail.com>
2025-03-11 12:07:45 +0700

    style: change delete button icon

--- script.js
+++ script.js
@@ -95,9 +95,19 @@ function addTask(taskText = null, save = true, checked = false) {
 
     // ❌ Delete Button
     const deleteButton = document.createElement('button');
-    deleteButton.textContent = '❌';
     deleteButton.classList.add('delete');
-    deleteButton.type = 'submit'; 
+    deleteButton.type = 'submit';
+    deleteButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M4 7l16 0" />
+            <path d="M10 11l0 6" />
+            <path d="M14 11l0 6" />
+            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+        </svg>
+    `;
+    
 
     form.onsubmit = function (e) {
         e.preventDefault();
